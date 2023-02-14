dc779fee8823d6a3a4dad6e5d845ce3fa6a2bca5
hiroshi <hiroshi.tan57@gmail.com>
2023-02-14 16:59:26 +0000

    Change synchronize to false

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ dotenv.config();
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
       logging: true,
     }),
     PubSubModule,
